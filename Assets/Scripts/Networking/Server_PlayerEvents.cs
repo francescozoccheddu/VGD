471dc25d8c8676d736d26e130e25f9c745ef6e7e
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-03-04 17:45:31 +0100

    Fix Server

--- Assets/Scripts/Networking/Server_PlayerEvents.cs
+++ Assets/Scripts/Networking/Server_PlayerEvents.cs
@@ -29,7 +29,7 @@ namespace Wheeled.Networking
             {
                 NetDataWriter writer = new NetDataWriter();
                 writer.Put(Message.Move);
-                writer.Put(m_player.id + 1);
+                writer.Put(m_player.id);
                 writer.Put(_node);
                 writer.Put(_input);
                 writer.Put(_calculatedSimulation);
