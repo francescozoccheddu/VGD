cd44ccdfc98e5889d4e1040ef7c5fc90cf173e70
Nicola <niko97.m@gmail.com>
2019-04-10 14:45:47 +0200

    Update particles

--- Assets/Scripts/Gameplay/Stage/RifleProjectileBehaviour.cs
+++ Assets/Scripts/Gameplay/Stage/RifleProjectileBehaviour.cs
@@ -4,22 +4,33 @@ namespace Wheeled.Gameplay.Stage
 {
     public sealed class RifleProjectileBehaviour : MonoBehaviour
     {
-        public ParticleSystem particleSystem;
-
+        public GameObject rifleHit;
+        public ParticleSystem particles;
+        
         private const float c_lifeTime = 1.0f;
+        private const float c_maxRayLenght = 40f;
+        private const int c_maxParticles = 100;
 
         private float m_elapsedTime;
 
         internal void Shoot(Vector3 _origin, Vector3 _end, bool _hit)
         {
             m_elapsedTime = 0.0f;
-            transform.position = _end;
-            Vector3 scale = new Vector3(1.0f, 1.0f, (_end - _origin).magnitude);
+            transform.position = _origin;
+            Vector3 ray = _end - _origin;
+            Vector3 scale = new Vector3(1.0f, 1.0f, ray.magnitude);
             transform.localScale = scale;
-            transform.localRotation = Quaternion.LookRotation(_origin - _end);
+            transform.localRotation = Quaternion.LookRotation(ray);
+
+            float rayDensity = Mathf.Clamp01(ray.magnitude / c_maxRayLenght);
+            int particleCount = Mathf.RoundToInt(rayDensity * c_maxParticles);
+
+            particles.emission.SetBurst(0, new ParticleSystem.Burst { count = particleCount });
+            
+
             if (_hit)
             {
-                particleSystem.Play();
+                Instantiate(rifleHit, _end, transform.rotation);
             }
         }
 
