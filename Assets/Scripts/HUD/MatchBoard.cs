12cf9182812e8ba93efe8357d1a0a64e0cb88d63
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-04-20 10:54:10 +0200

    Add MatchBoard class

---
+++ Assets/Scripts/HUD/MatchBoard.cs
@@ -0,0 +1,74 @@
+﻿using Wheeled.Core.Utils;
+using Wheeled.Gameplay.Action;
+using Wheeled.Gameplay.Player;
+
+namespace Wheeled.Assets.Scripts.HUD
+{
+    internal sealed class MatchBoard
+    {
+        #region Public Interfaces
+
+        public interface IEvent
+        {
+        }
+
+        #endregion Public Interfaces
+
+        #region Public Structs
+
+        public struct JoinEvent : IEvent
+        {
+            #region Public Fields
+
+            public IReadOnlyPlayer player;
+
+            #endregion Public Fields
+        }
+
+        public struct KillEvent : IEvent
+        {
+            #region Public Fields
+
+            public IReadOnlyPlayer killer;
+            public IReadOnlyPlayer victim;
+            public OffenseType offenseType;
+
+            #endregion Public Fields
+        }
+
+        public struct QuitEvent : IEvent
+        {
+            #region Public Fields
+
+            public IReadOnlyPlayer player;
+
+            #endregion Public Fields
+        }
+
+        #endregion Public Structs
+
+        #region Private Fields
+
+        private readonly LinkedListHistory<double, IEvent> m_history;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public MatchBoard()
+        {
+            m_history = new LinkedListHistory<double, IEvent>();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void Put(double _time, IEvent _event)
+        {
+            m_history.Add(_time, _event);
+        }
+
+        #endregion Public Methods
+    }
+}
