fe69e206625b4c32c13d1de88428997971c6bac4
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-04-24 18:07:55 +0200

    Update rifle ray

---
+++ Assets/Scripts/Gameplay/Offense/RifleRayBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Wheeled.Gameplay.Stage
+{
+    public sealed class RifleRayBehaviour : MonoBehaviour
+    {
+        #region Public Methods
+
+        public void Destroy()
+        {
+            Destroy(gameObject);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Assets/Scripts/HUD/QuitBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Wheeled.Core;
+
+namespace Wheeled.Assets.Scripts.HUD
+{
+    public sealed class QuitBehaviour : MonoBehaviour
+    {
+        #region Public Fields
+
+        public Text text;
+        public CanvasGroup group;
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private const float c_fadeOutDuration = 4.0f;
+        private const int c_quitTime = 3;
+        private float m_elapsedTime;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        private void Update()
+        {
+            if (Input.GetButton("Quit"))
+            {
+                m_elapsedTime += Time.deltaTime;
+                text.text = string.Format("Leaving game in {0} seconds", c_quitTime - Mathf.FloorToInt(m_elapsedTime));
+                if (m_elapsedTime >= c_quitTime)
+                {
+                    GameLauncher.Instance.QuitGame();
+                }
+                group.alpha = 1.0f;
+            }
+            else
+            {
+                m_elapsedTime = 0.0f;
+                text.text = "Hold <b>ESC</b> to leave game";
+                group.alpha = Mathf.Max(0.0f, group.alpha - Time.deltaTime / c_fadeOutDuration);
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
