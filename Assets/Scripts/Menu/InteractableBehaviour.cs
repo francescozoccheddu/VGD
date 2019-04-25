23c28b0576192ae48fa06ce993049eedffdca513
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-04-25 16:20:47 +0200

    Add settings screen

---
+++ Assets/Scripts/Menu/InteractableBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wheeled.Menu
+{
+    public sealed class InteractableBehaviour : MonoBehaviour
+    {
+        #region Public Fields
+
+        public Shadow shadow;
+        public new RectTransform transform;
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private const float c_disabledScale = 0.65f;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        public void SetEnabled(bool _enabled)
+        {
+            if (transform != null)
+            {
+                transform.localScale = _enabled ? new Vector3(1, 1, 1) : new Vector3(c_disabledScale, c_disabledScale, c_disabledScale);
+            }
+            if (shadow != null)
+            {
+                shadow.enabled = _enabled;
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Assets/Scripts/Menu/SettingsBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wheeled.Menu
+{
+    public sealed class SettingsBehaviour : MonoBehaviour
+    {
+        #region Public Fields
+
+        public ToggleGroup headGroup;
+        public ToggleGroup colorGroup;
+        public InputField nameField;
+        public Button saveButton;
+        public InteractableBehaviour interactable;
+
+        #endregion Public Fields
+
+        #region Public Methods
+
+        public void TextChanged(string _text)
+        {
+            bool enabled = !string.IsNullOrEmpty(_text);
+            interactable.SetEnabled(enabled);
+            saveButton.interactable = enabled;
+        }
+
+        public void Save()
+        {
+        }
+
+        #endregion Public Methods
+    }
+}
