25a910ce461d58638eacffafbaef31ebab42cddf
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-04-26 13:51:40 +0200

    Add join screen

---
+++ Assets/Scripts/Menu/LoadingLabelBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wheeled.Menu
+{
+    internal sealed class LoadingLabelBehaviour : MonoBehaviour
+    {
+        #region Public Fields
+
+        public Text text;
+        public string message;
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private const int c_totalDotCount = 3;
+        private const float c_period = 1 / 2.0f;
+
+        private int m_dotCount;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        private void OnEnable()
+        {
+            InvokeRepeating(nameof(UpdateText), c_period, c_period);
+            m_dotCount = 0;
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            text.text = message + new string('.', m_dotCount);
+            m_dotCount = (m_dotCount + 1) % (c_totalDotCount + 1);
+        }
+
+        private void OnDisable()
+        {
+            CancelInvoke();
+        }
+
+        #endregion Private Methods
+    }
+}
