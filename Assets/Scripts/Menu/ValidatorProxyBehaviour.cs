e980455322c7c69f888fd4ba32715549a2c2ccc4
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-05-11 21:09:18 +0200

    Update menu colors

---
+++ Assets/Scripts/Menu/ValidatorProxyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Wheeled.Menu
+{
+    public sealed class ValidatorProxyBehaviour : ValidatorBehaviour
+    {
+        
+        public void Validate(bool _isValid)
+        {
+            validated.Invoke(_isValid);
+        }
+
+    }
+}
