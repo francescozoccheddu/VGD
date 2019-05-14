466452fc9bd4f48d485d82ba46768dd510b214d8
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-05-14 20:29:35 +0200

    Add death camera prefab

--- Assets/Scripts/Gameplay/Scene/DeathCameraBehaviour.cs
+++ Assets/Scripts/Gameplay/Scene/DeathCameraBehaviour.cs
@@ -4,21 +4,13 @@ namespace Wheeled.Gameplay.Scene
 {
     public sealed class DeathCameraBehaviour : MonoBehaviour
     {
-        #region Public Fields
 
-        public new Camera camera;
-        public AudioListener audioListener;
-
-        #endregion Public Fields
-
-        #region Public Methods
+        public new GameObject camera;
 
         public void SetEnabled(bool _enabled)
         {
-            camera.enabled = _enabled;
-            audioListener.enabled = _enabled;
+            camera.SetActive(_enabled);
         }
 
-        #endregion Public Methods
     }
 }
