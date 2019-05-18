1820e5302ac15254ac9f0bf8e2ef0a658e6f67fa
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-05-18 14:40:56 +0200

    Update post process stack

---
+++ Assets/Scripts/Scene/PostProcessingBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Wheeled.Scene
+{
+    public sealed class PostProcessingBehaviour : MonoBehaviour
+    {
+
+        public PostProcessVolume volume;
+
+        public int minQuality;
+
+        private void Set()
+        {
+            volume.enabled = QualitySettings.GetQualityLevel() >= minQuality;
+        }
+
+        private void OnEnable()
+        {
+            Set();
+        }
+
+        private void OnValidate()
+        {
+            Set();
+        }
+
+    }
+}
