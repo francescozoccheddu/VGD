56724d7005232a100d59766512234f0afdd675cc
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-05-19 19:40:16 +0200

    Add scoreboard join info

---
+++ Assets/Scripts/UI/HUD/ScoreBoard/ScoreBoardRoomInfoBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+using Wheeled.Core;
+
+namespace Wheeled.UI.HUD
+{
+    public class ScoreBoardRoomInfoBehaviour : MonoBehaviour
+    {
+
+        public Text text;
+
+        private void OnEnable()
+        {
+            IPEndPoint endPoint = GameManager.Current.Room.endPoint;
+            text.text = string.Format("<color=\"#FFF7\">join at </color>{0}<color=\"#FFF7\">:</color>{1}", endPoint.Address.MapToIPv4(), endPoint.Port);
+        }
+
+    }
+}
