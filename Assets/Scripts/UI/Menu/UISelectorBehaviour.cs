6e2761655e7330b02b84faae8862bf012e40b0db
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-05-28 21:45:05 +0200

    Update input settings

---
+++ Assets/Scripts/UI/Menu/UISelectorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Wheeled.UI.Menu
+{
+    public sealed class UISelectorBehaviour : MonoBehaviour
+    {
+
+        public string[] selectionButtons;
+
+        private void Update()
+        {
+            if (selectionButtons.Select(_n => Input.GetButtonDown(_n) || Input.GetAxisRaw(_n) != 0.0f).Any(_b => _b))
+            {
+                GameObject selected = EventSystem.current.currentSelectedGameObject;
+                if (selected?.activeInHierarchy != true)
+                {
+                    GameObject.FindGameObjectWithTag("UIDefaultSelection")?.GetComponentInChildren<Selectable>()?.Select();
+                }
+            }
+        }
+
+    }
+}
